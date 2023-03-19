65d4097dbe3bdac9f91ccb9a59a4d34e7e712643
徐锐 <2603029264@qq.com>
2023-03-20 00:06:12 +0800

    clone拖拽

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,38 @@ router.get('/', async (ctx, next) => {
   next()
 })
 
+router.get('/info', async (ctx) => {
+  const { num } = ctx.query
+  console.log(num)
+  ctx.body = {
+    qq: '18755603174',
+    birth: '1999-09-16'
+  }
+})
+
+router.post('/permission', async (ctx) => {
+  console.log(ctx.request.body)
+  ctx.body = {
+    msg: 'success',
+    data: {
+      name: 'admin',
+      age: 10,
+      sex: 'male',
+      status: false,
+    }
+  }
+})
+
+router.post('/submit', async (ctx) => {
+  const { name, age, sex, status, qq, birth } = ctx.request.body
+  ctx.body = {
+    name,
+    age,
+    sex,
+    status,
+    qq,
+    birth,
+    tag: 'success'
+  }
+})
 module.exports = router
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,4 +4,5 @@ router.prefix('/users')
 
 router.get('/', getUserInfo)
 
+
 module.exports = router
